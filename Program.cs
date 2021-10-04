c45442e483f8cbb2b901732a6c7064074a16b6c8
Allie Scherf <ascherf2@my.wctc.edu>
2021-10-04 18:46:33 -0500

    created project & cleaned up code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.IO;
 
 namespace MovieLibrary
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+            // create instance of Logger
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+
+        //start logging
+            logger.Info("Test");
+
+            //initiate stream reader
+
+            try
+            {
+                Console.WriteLine("Hello, how can I help?");
+                Console.WriteLine("Please enter 1 for Movie library:");
+                //Display library in a pleasing fashion
+                Console.WriteLine("Please enter 2 to add to Movie library");
+                //Add to not write over 
+                Console.WriteLine("Please enter any other key to exit.");
+
+                //readline here, convert toint32
+
+                //after every new entry movie id = new movie id ++
+                //dont give option to enter movie ID, self assigned by program
+                //menu example: "Please add data to Movie ID 00000:"
+
+                //option 1 read through library and display 
+                using (StreamReader sr = new StreamReader("movies.csv"))
+                {
+                    string line;
+                
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please try again, error:");
+                Console.WriteLine(e.Message);
+            }
+            //     //exit/ close file
+            //     //Unit testing
+            //     //Git uploads
         }
     }
 }
